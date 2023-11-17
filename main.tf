3f85f4fa8026b0c2b8cca3cb3e684da1b57c3111
Santiago Gallego Henao <109182667+mowsix@users.noreply.github.com>
2023-11-16 22:16:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,20 +5,25 @@ terraform {
       version = "~> 4.16"
     }
   }
+
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "ASIARMWZPIP7KVXPSGNM"
-  secret_key = "rbgNIT0CP/KDlJjTCpA10dvGF32wC4iAOfyPT2cI"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-08f9c2c69057b8267"]
+  associate_public_ip_address = true
 
+  provisioner "local-exec"{
+    command = "sudo ./script.sh"
+    interpreter = [ "*/bin/bash", "-c"]
+  }
   tags = {
-    Name = "EjemploIaCSalon"
+    Name = "SantiagoGallego"
   }
 }
